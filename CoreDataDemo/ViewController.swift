b42af49b765e9bc04135be8f13f763bdac21235a
Jatin Arora <jatinarora269@gmail.com>
2018-04-13 20:49:42 +0530

    Add PersistenceController and TableView

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+enum Section: Int {
+    
+    case injectObjects
+    case fetchRequest
+    case faults
+    case performance
+    
+    case _count
+    
+}
+
 final class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,47 @@ final class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let section = Section(rawValue: indexPath.row) else {
+            fatalError()
+        }
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        switch section {
+        case .injectObjects:
+            cell.textLabel?.text = "Insert Objects"
+        case .fetchRequest:
+            cell.textLabel?.text = "Fetch Request"
+        case .faults:
+            cell.textLabel?.text = "Faults"
+        case .performance:
+            cell.textLabel?.text = "Performance"
+        default:
+            fatalError()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Section._count.rawValue
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+}
+
 
